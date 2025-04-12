e67e22109a7b95239fea498be54adbf19a195668
jorge <jovisu2@hotmail.es>
2025-04-11 21:26:44 -0500

    Se hicieron los ojos animados en la barra de menu y se le hicieron animaciones al cursor

--- validacion.js
+++ validacion.js
@@ -23,3 +23,52 @@ spans.forEach((span) =>{
     span.innerHTML = span.dataset.width;
 })
 
+document.querySelector('body').addEventListener('mousemove',
+    eyeball);
+
+    function eyeball() {
+        const eye = document.querySelectorAll('.eye');
+        eye.forEach(function (eye) {
+            let x = (eye.getBoundingClientRect().left) + eye.clientHeight / 2;
+            let y = (eye.getBoundingClientRect().top) + eye.clientHeight / 2;
+
+            let radian = Math.atan2(event.pageX - x, event.pageY - y);
+            let rotation = (radian * (180 / Math.PI) * -1) + 360;
+            eye.style.transform = "rotate(" + rotation + "deg)";
+    });
+}
+
+let lastMouseX = 0;
+let lastMouseY = 0;
+const separation = 10;
+
+function crearBinario (x, y){
+    const binario = document.createElement("div");
+    binario.textContent = Math.random() > 0.5 ? "1" : "0";
+    binario.classList.add("binario");
+    document.body.appendChild(binario);
+    binario.style.position = `absolute`;
+    binario.style.left = `${x}px`;
+    binario.style.top = `${y}px`;
+
+    setTimeout(() => binario.remove(), 3000);
+}
+
+document.addEventListener("mousemove", (e) => {
+const mouseX = e.clientX + window.scrollX;
+const mouseY = e.clientY + window.scrollY;
+const distX = Math.abs(mouseX - lastMouseX);
+const distY = Math.abs(mouseY - lastMouseY);
+
+if (distX >= separation || distY >= separation) {
+    crearBinario(mouseX, mouseY);
+    lastMouseX = mouseX;
+    lastMouseY = mouseY;
+}
+})
+
+var cursor = document.querySelector('.cursor');
+var cursor2 = document.querySelector('.cursor2');
+document.addEventListener('mousemove', function(e) {
+    cursor.style.cssText = cursor2.style.cssText = 'left:' + e.clientX + 'px; top:' + e.clientY + 'px;';
+});
