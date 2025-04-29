c4314e5c8b2f6849993f560740d451aa9bea90f6
jorge <jovisu2@hotmail.es>
2025-04-29 18:03:47 -0500

    Se creo el boton para cambiar la pagina de idioma

--- validacion.js
+++ validacion.js
@@ -78,3 +78,120 @@ var cursor2 = document.querySelector('.cursor2');
 document.addEventListener('mousemove', function(e) {
     cursor.style.cssText = cursor2.style.cssText = 'left:' + e.clientX + 'px; top:' + e.clientY + 'px;';
 });
+
+//cambio de idioma
+const toggleBtn = document.getElementById("languageToggle");
+
+// Elementos de texto que cambiarán
+const traducciones = {
+    es: {
+        saludo: "Hola, mi nombre es Jorge Augusto Villada Carvajal y construyo paginas web",
+        subtitulo: "Soy formado en Analisis y Desarrollo de Sistemas de Informacion y actualmente estoy participando del proyecto Oracle ONE en Alura Latam.",
+        contacto: "¿Quieres contactarme?",
+        formulario: "Complete el siguiente formulario y me pondré en contacto con usted lo antes posible.",
+        boton: "Enviar mensaje",
+        sobreMi: "Sobre mi",
+        skills: "Habilidades",
+        hobbies: "Pasatiempos",
+        formacion: "Formación",
+        xp:"Proyectos",
+        tituloSobreMi1: "Sobre mí",
+        parrafo: "Soy estudiante de Ingeniería de Sistemas, busco oportunidad para adquirir experiencia en el área del desarrollo web con FlutterFlow versión 3.19.1, JavaScript, HTML5, Css3, Git y GitHub u otras tecnologías donde pueda desarrollar mis conocimientos técnicos y crecer junto a la empresa y a mis compañeros de trabajo también poseo conocimiento en manejo de bases de datos como Firebase y plataformas como Slack, Figma, Mural y Jira para metodologías Scrum.",
+        parrafo1:"Soy una persona curiosa y me gusta resolver problemas. Aprender hace parte de mí día a día y los desafíos me motivan a encontrar las mejores soluciones.",
+        parrafo2:"Busco siempre trabajar en equipo, cooperando con todos y apoyando de la mejor manera posible, con grande satisfacción en aprender y enseñar.",
+        titulodehabilidades: "Habilidades",
+        titulodepasatiempos: "Pasatiempos",
+        tituloformacionacademica: "Formación académica",
+        subtituloformacionacademica: "Tecnologo en Análisis y Desarrollo de Sistemas de Informacion",
+        titulodeformacionacademica1: "Pregrado en Ingenieria de Sistemas",
+        parrafodeformacionacademica1: "En curso - UNAD",
+        experiencia: "Experiencia Profesional",
+        experiencia1: "Experiencia Profesional",
+        experiencetitle1: "CRM para empresa de Productos organicos",
+        experiencedetitle: "Calendario de agendamiento",
+        botonrepositorio: "Repositório",
+        botonverrepositorio: "Ver demo",
+        titulodeformulario: "Contacto",
+        nombreformulario:"Nombre:",
+        emailformulario:"Correo electrónico:",
+        asuntoformulario:"Asunto:",
+        mensajeformulario:"Mensaje:",
+        footerclas:"Desarrollado por Jorge Augusto Villada Carvajal",
+       
+    },
+    en: {
+        saludo: "Hi, my name is Jorge Augusto Villada Carvajal and I build websites",
+        subtitulo: "I graduated in Systems Information Analysis and Development and I am currently part of the Oracle ONE project at Alura Latam.",
+        contacto: "Do you want to contact me?",
+        formulario: "Fill out the form below and I will get in touch with you as soon as possible.",
+        boton: "Send message",
+        sobreMi: "About me",
+        skills: "Skills",
+        hobbies: "Hobbies",
+        formacion: "Training",
+        xp:"Projects",
+        tituloSobreMi1: "About me",
+        parrafo: "I am a Systems Engineering student, looking for an opportunity to gain experience in the area of ​​​​web development with FlutterFlow version 3.19. 1, JavaScript, HTML5, Css3, Git and GitHub or other technologies where I can develop my technical knowledge and grow together with the company and my coworkers. I also have knowledge in managing databases such as Firebase and platforms such as Slack, Figma, Mural and Jira for Scrum methodologies..",
+        parrafo1:"I'm a curious person and I like to solve problems. Learning is part of my daily life, and challenges motivate me to find the best solutions.",
+        parrafo2:"I always seek to work as a team, cooperating with everyone and supporting them in the best way possible, with great satisfaction in learning and teaching.",
+        titulodehabilidades: "Skills",
+        titulodepasatiempos: "Hobbies",
+        tituloformacionacademica: "Academic training",
+        subtituloformacionacademica: "Technologist in analysis and development of information Systems",
+        titulodeformacionacademica1: "Undergraduate in Systems Engineering",
+        parrafodeformacionacademica1: "En curso - UNAD",
+        experiencia: "Professional Experience",
+        experiencia1: "Professional Experience",
+        experiencetitle1: "CRM for an organic products company",
+        experiencedetitle: "Scheduling calendar",
+        botonrepositorio: "Repository",
+        botonverrepositorio: "See demo",
+        titulodeformulario: "Contact",
+        nombreformulario:"Name:",
+        emailformulario:"Email:",
+        asuntoformulario:"Affair:",
+        mensajeformulario:"Message:",
+        footerclas:"Developed by Jorge Augusto Villada Carvajal",
+    }
+};
+
+let idioma = "es";
+
+toggleBtn.addEventListener("click", () => {
+    idioma = idioma === "es" ? "en" : "es";
+
+    document.querySelector(".title__bio").textContent = traducciones[idioma].saludo;
+    document.querySelector(".title__subsection").textContent = traducciones[idioma].subtitulo;
+    document.querySelector(".formcontato__subtext").textContent = traducciones[idioma].contacto;
+    document.querySelector(".formcontato__subtexte").textContent = traducciones[idioma].formulario;
+    document.querySelector(".formcontato__botao").textContent = traducciones[idioma].boton;
+    document.getElementById("tituloSobreMi").textContent = traducciones[idioma].sobreMi;
+    document.getElementById("tituloSkills").textContent = traducciones[idioma].skills;
+    document.getElementById("tituloHobbies").textContent = traducciones[idioma].hobbies;
+    document.getElementById("tituloFormacion").textContent = traducciones[idioma].formacion;
+    document.getElementById("tituloProyectos").textContent = traducciones[idioma].xp;
+    document.getElementById("tituloSobreMi1").textContent = traducciones[idioma].tituloSobreMi1;
+    document.getElementById("parrafo").textContent = traducciones[idioma].parrafo;
+    document.getElementById("parrafo1").textContent = traducciones[idioma].parrafo1;
+    document.getElementById("parrafo2").textContent = traducciones[idioma].parrafo2;
+    document.getElementById("titulodehabilidades").textContent = traducciones[idioma].titulodehabilidades;
+    document.getElementById("titulodepasatiempos").textContent = traducciones[idioma].titulodepasatiempos;
+    document.getElementById("tituloformacionacademica").textContent = traducciones[idioma].tituloformacionacademica;
+    document.getElementById("subtituloformacionacademica").textContent = traducciones[idioma].subtituloformacionacademica;
+    document.getElementById("titulodeformacionacademica1").textContent = traducciones[idioma].titulodeformacionacademica1;
+    document.getElementById("parrafodeformacionacademica1").textContent = traducciones[idioma].parrafodeformacionacademica1;
+    document.getElementById("Experiencia").textContent = traducciones[idioma].experiencia;
+    document.getElementById("Experiencia1").textContent = traducciones[idioma].experiencia1;
+    document.getElementById("experiencetitle1").textContent = traducciones[idioma].experiencetitle1;
+    document.getElementById("experiencedetitle").textContent = traducciones[idioma].experiencedetitle;
+    document.getElementById("titulodeformulario").textContent = traducciones[idioma].titulodeformulario;
+    document.getElementById("nombreformulario").textContent = traducciones[idioma].nombreformulario;
+    document.getElementById("emailformulario").textContent = traducciones[idioma].emailformulario;
+    document.getElementById("asuntoformulario").textContent = traducciones[idioma].asuntoformulario;
+    document.getElementById("mensajeformulario").textContent = traducciones[idioma].mensajeformulario;
+    document.getElementById("footerclas").textContent = traducciones[idioma].footerclas;
+    document.querySelector("botonrepositorio").textContent = traducciones[idioma].botonrepositorio;
+    document.querySelector("botonverrepositorio").textContent = traducciones[idioma].botonverrepositorio;
+    
+
+});
